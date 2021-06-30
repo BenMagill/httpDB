afffb7f3a364f18c49a4df6e6b0976dd5dc6be8e
Ben <inbox@benmagill.co.uk>
2021-06-30 15:16:41 +0100

    Improve logger

--- src/logger.ts
+++ src/logger.ts
@@ -14,20 +14,28 @@ export enum Scope {
     UNKNOWN = "UNKNOWN"
 }
 
-var currDay = new Date().toISOString().split(":")[0]
-var stream = createWriteStream(`${process.cwd()}/logs/${currDay}.log`, {flags: "a"})
+const getFileName = (dateISO: string) => {
+    return dateISO.split(":")[0]
+}
+
+const getPath = () => {
+    return `${process.cwd()}/logs/${getFileName(new Date().toISOString())}.log`
+}
+
+var currDay = getFileName(new Date().toISOString())
+var stream = createWriteStream(getPath(), {flags: "a"})
 
 export const log = (type: Type, scope: Scope, message: string) => {
     const date = new Date().toISOString()
     const text = `[${date}] [${type}] [${scope}] ${message}`
     console.log(text)
     
-    const day = date.split(":")[0]
+    const day = getFileName(date)
     if (currDay !== day) {
         // need to swap stream to new file
         currDay = day
         stream.end()
-        stream = createWriteStream(`${process.cwd()}/logs/${day}.log`, {flags: "a"})
+        stream = createWriteStream(getPath(), {flags: "a"})
     } 
 
     stream.write(text + "\n")
